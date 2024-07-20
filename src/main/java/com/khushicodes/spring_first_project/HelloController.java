7c4317127bc607c21187b5f12c927ee358940bf0
kckhushichauhan <kc.shreegajanan@gmail.com>
2024-07-20 17:09:49 +0530

    Add files via upload
    This is the Demo SpringBoot Project.

---
+++ src/main/java/com/khushicodes/spring_first_project/HelloController.java
@@ -0,0 +1,8 @@
+package com.khushicodes.spring_first_project;
+
+public class HelloController {
+	public static void main(String[] args) {
+		System.out.println("Shree Gajanan Technologies");
+	}
+
+}
